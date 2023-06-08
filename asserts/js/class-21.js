591c9b7869da0f33a3a3bd56c4e17afdb2ee0105
zohirul22 <shahin57islam@gmail.com>
2023-06-08 15:11:32 +0600

    class 21

---
+++ asserts/js/class-21.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+
+  
+    nav: false,
+    dots: true,
+    loop: true,
+    autoplay:false,
+   
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 1
+        }
+    }
+})
